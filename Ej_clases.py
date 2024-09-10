382ed9b9ae780da872657c9c3ea484485680b5d4
Isaac-Jimenez-1347 <164533988+Isaac-Jimenez-1347@users.noreply.github.com>
2024-09-10 12:13:28 -0600

    Add files via upload

--- Ej_clases.py
+++ Ej_clases.py
@@ -2,10 +2,12 @@ print("introduccion a clases")
 class animal:
     edad=3
     raza="pitbul terrier"
-    comida="croquetas"
+    comida="humanos"
+    nombre="el men"
     def come(self):
         print("el perro come "+self.comida)
 perro=animal()
+print(f"el perro se llama {perro.nombre}" ) 
 print(f"el perro tiene {perro.edad} años" ) 
 print(f"el perro es raza {perro.raza}" )
 perro.come()
